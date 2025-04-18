67d4c57f640d782f9bd75950810d8f9c727edbb9
sharabh mishra <sharabhmishra15@gmail.com>
2025-04-19 00:47:09 +0530

    adding values to a dictionary

---
+++ 10_hour_video/practise_set_5/prob_4.py
@@ -0,0 +1,10 @@
+# Create an empty dictionary. Allow 4 friends to enter their favorite language as
+# value and use key as their names. Assume that the names are unique.
+
+dict1 = {}
+dict1["sharabh"] = str(input("sharabh enter your favorite language: "))
+dict1["tasmiah"] = str(input("tasmiah your favorite language: "))
+dict1["harshit"] = str(input("harshit your favorite language: "))
+dict1["jai"] = str(input("jai your favorite language: "))
+
+print("The favorite languages of the friends are: ", dict1)
