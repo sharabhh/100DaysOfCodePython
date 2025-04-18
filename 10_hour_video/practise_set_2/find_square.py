460e14abb2b8a0279fc0f2a4a4cd3e52afda9be6
sharabh mishra <sharabhmishra15@gmail.com>
2025-04-18 16:39:27 +0530

    good morning text

--- 10_hour_video/practise_set_2/find_square.py
+++ 10_hour_video/practise_set_2/find_square.py
@@ -1,3 +1,3 @@
 a = int(input("Enter a number: "))
-print("The square of the number is: ", a*a)
+print("The square of the number is: ", a^2)
 # The above code takes an integer input from the user and prints its square.
---
+++ 10_hour_video/practise_set_3/prob_1.py
@@ -0,0 +1,4 @@
+# Write a python program to display a user entered name followed by Good Afternoon using input () function.
+
+a = str(input("Enter your name: ")).capitalize()
+print(a + ", Good Afternoon")
