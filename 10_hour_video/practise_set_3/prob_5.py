b43611ade2241c59a4053115fce84b360ec358d3
sharabh mishra <sharabhmishra15@gmail.com>
2025-04-18 18:05:16 +0530

    added escape sequence

---
+++ 10_hour_video/practise_set_3/prob_5.py
@@ -0,0 +1,4 @@
+# Write a program to format the following letter using escape sequence characters.
+letter = "Dear Harry, this python course is nice. Thanks!"
+newLetter = letter.replace(". ","\n")
+print(newLetter)
