9619e62aa3a4d0a0766e7cd4dd8830c56457c192
sharabh mishra <sharabhmishra15@gmail.com>
2025-04-18 16:43:26 +0530

    string formatting

---
+++ 10_hour_video/practise_set_3/prob_2.py
@@ -0,0 +1,11 @@
+# Write a program to fill in a letter template given below with name and date.
+# letter = '''
+# Dear <|Name|>,
+# You are selected!
+# <|Date|>
+# '''
+
+name = str(input("Enter your name: ")).capitalize()
+date = str(input("Enter the date: "))
+letter = "Dear " + name + ",\nYou are selected!\n" + date
+print(letter)
