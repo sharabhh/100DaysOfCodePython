c521e220c797a0ccaf3c1c86f76dcb0808fe8250
sharabh mishra <sharabhmishra15@gmail.com>
2025-04-18 18:09:45 +0530

    list append

---
+++ 10_hour_video/practise_set_4/prob_1.py
@@ -0,0 +1,18 @@
+# Write a program to store seven fruits in a list entered by the user.
+list = []
+first = str(input("Enter first fruit: "))
+list.append(first)
+second = str(input("Enter second fruit: "))
+list.append(second)
+third = str(input("Enter third fruit: "))
+list.append(third)
+fourth = str(input("Enter fourth fruit: "))
+list.append(fourth)
+fifth = str(input("Enter fifth fruit: "))
+list.append(fifth)
+sixth = str(input("Enter sixth fruit: "))
+list.append(sixth)
+seventh = str(input("Enter seventh fruit: "))
+list.append(seventh)
+
+print("The fruits are: ", list)
