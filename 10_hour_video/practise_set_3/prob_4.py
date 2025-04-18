a18a4dbd8fc078e4e7bef96b22efdc79902f1ab8
sharabh mishra <sharabhmishra15@gmail.com>
2025-04-18 16:47:15 +0530

    replace double space with single

---
+++ 10_hour_video/practise_set_3/prob_4.py
@@ -0,0 +1,5 @@
+# Replace the double space from problem 3 with single spaces.
+l = "hi  there  how  are  you"
+m = l.replace("  ", " ")
+print(m) # This will replace all occurrences of double space with single space
+# # The replace() method is used to replace a specified phrase with another specified phrase
