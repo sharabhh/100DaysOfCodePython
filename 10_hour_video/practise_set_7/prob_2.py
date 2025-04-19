04f029928e19ab49b22df68dc52d405bd5d097b2
sharabh mishra <sharabhmishra15@gmail.com>
2025-04-19 13:08:45 +0530

    greet with conditional

---
+++ 10_hour_video/practise_set_7/prob_2.py
@@ -0,0 +1,7 @@
+# Write a program to greet all the person names stored in a list ‘l’ and which starts
+# with S
+l = ["Harry", "Soham", "Sachin", "Rahul"]
+
+for i in l:
+    if(i.startswith("S")):
+        print(f"Good morning, {i}")
