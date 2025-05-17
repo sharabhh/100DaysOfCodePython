f01993160fe3b5aacef521dcc4b45e7d63892106
sharabh mishra <sharabhmishra15@gmail.com>
2025-05-17 18:09:52 +0530

    staticmethod in class

---
+++ 10_hour_video/practise_set_10/prob_4.py
@@ -0,0 +1,27 @@
+# Add a static method in problem 2, to greet the user with hello.
+# Write a class “Calculator” capable of finding square, cube and square root of a
+# number.
+
+class Calculator:
+
+    def __init__(self, num):
+        self.num = num
+
+    def square(self):
+        print(f"The square of {self.num} is {pow(self.num, 2)}")
+    
+    def cube(self):
+        print(f"The cube of {self.num} is {pow(self.num, 3)}")
+    
+    def sqrt(self):
+        print(f"The squareroot of {self.num} is {pow(self.num, 1/2)}")
+
+    @staticmethod
+    def greet():
+        print("Hola! amigos")
+    
+obj1 = Calculator(4)
+obj1.square()
+obj1.cube()
+obj1.sqrt()
+obj1.greet()
